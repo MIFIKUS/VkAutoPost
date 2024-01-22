cff3a013df8e9b8a77c9ea85dad654792f4a70c0
MIFIKUS <alchoholru.2.4.5@gmail.com>
2024-01-22 11:18:47 +0300

    22223

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import telebot
 log = Logs()
 
 def _get_token():
-    with open('\\service_files\\tg.json') as tg_cnf:
+    with open('service_files\\tg.json') as tg_cnf:
         return json.load(tg_cnf).get('API_KEY')
 def _get_admin_id():
-    with open('\\service_files\\tg.json') as tg_cnf:
+    with open('service_files\\tg.json') as tg_cnf:
         return json.load(tg_cnf).get('ADMIN_ID')
 
 log.log('Бот запущен')
